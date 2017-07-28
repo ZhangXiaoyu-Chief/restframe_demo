e694516d6a3625d060e4313c1090bfeb14c10353
zhangxiaoyu <zhangxiaoyu@afcat.com.cn>
2017-07-28 16:11:35 +0800

    serializer

---
+++ demo/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from demo.models import Book,Publisher
+
+
+class PublisherSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField(max_length=30)
+    address = serializers.CharField(max_length=50)
+    city = serializers.CharField(max_length=60)
+    state_province = serializers.CharField(max_length=30)
+    country = serializers.CharField(max_length=50)
+    website = serializers.URLField()
+
+    def create(self, validated_data):
+        validated_data.pop("id")
+        return Publisher.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get("name", instance.name)
+        instance.address = validated_data.get("address", instance.address)
+        instance.city = validated_data.get("city", instance.city)
+        instance.state_province = validated_data.get("state_province", instance.state_province)
+        instance.country = validated_data.get("country", instance.country)
+        instance.website = validated_data.get("website", instance.website)
+        instance.save()
+        return instance
--- restframe_demo/settings.py
+++ restframe_demo/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'demo.apps.DemoConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
